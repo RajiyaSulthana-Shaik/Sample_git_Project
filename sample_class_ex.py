f1dc967afea0578bfdf832d26618d7191bfeff39
ShaikRajiyaSulthana <rajiyasulthana2001@gmail.com>
2024-08-11 10:55:44 +0530

    sampleclass python file

--- sample_class_ex.py
+++ sample_class_ex.py
@@ -1,7 +1,7 @@
 class Student:
-	def Course():
+	def Course(self):
 		print("Full Stack Python")
-	def No_of_stu():
+	def No_of_stu(self):
 		print("Total Students are 15")
 stu = Student()
 stu.Course()
